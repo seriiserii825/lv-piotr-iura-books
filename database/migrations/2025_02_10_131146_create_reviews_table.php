b9c511f8b82339d7fd69ebc93815b7fd873d9f59
serii burduja <seriiburduja@gmail.com>
2025-02-11 09:49:59 +0200

    feat: migrate books and review

--- database/migrations/2025_02_10_131146_create_reviews_table.php
+++ database/migrations/2025_02_10_131146_create_reviews_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->text('review');
+            $table->unsignedBigInteger('rating');
             $table->timestamps();
         });
     }
