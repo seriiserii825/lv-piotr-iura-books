628c5e94e161bae4a1d7e957c0db1eaa9ae1da27
serii burduja <seriiburduja@gmail.com>
2025-02-19 09:12:25 +0200

    feat: date range filter

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -26,19 +26,32 @@ class Book extends Model
 
     public function scopePopular(Builder $query, $from = null, $to = null)
     {
-        return $query->withCount(['reviews' => function (Builder $q) use ($from, $to) {
-            if ($from && !$to) {
-                $q->where('created_at', '>=', $from);
-            } elseif (!$from && $to) {
-                $q->where('created_at', '<=', $to);
-            } elseif ($from && $to) {
-                $q->whereBetween('created_at', [$from, $to]);
-            }
-        }])->orderBy('reviews_count', 'desc');
+        return $query->withCount(
+            ['reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)]
+        )->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query)
+    public function scopeHighestRated(Builder $query, $from = null, $to = null)
     {
-        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
+        return $query->withAvg(
+            ['reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)],
+            'rating'
+        )->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    private function dateRangeFilter(Builder $q, $from, $to)
+    {
+        if ($from && !$to) {
+            $q->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $q->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $q->whereBetween('created_at', [$from, $to]);
+        }
+    }
+
+    public function scopeMinReviews(Builder $query, $count)
+    {
+        return $query->withCount('reviews')->having('reviews_count', '>=', $count);
     }
 }
